7d52b8aa8f7df2a9149d0da32002ff42c595bb8f
Che Sergiu <37384588+CHESERGIU@users.noreply.github.com>
2020-07-03 10:57:05 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace App16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputData = Console.ReadLine(); 
+            double first = Convert.ToSingle(inputData);
+            inputData = Console.ReadLine(); 
+            double second = Convert.ToSingle(inputData);
+            double min = Math.Round((second < first ? second : first), 2);
+            Console.WriteLine(min);
+        }
+    }
+}
